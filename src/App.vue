3e7c5b0501ed31134bacf42b72a95c44c8e92079
Elvan B. Budiongan <ebbudiongan@vauldex.com>
2024-10-04 10:22:46 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AddList from './components/AddList.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    AddList
+  }
+})
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <h1>My item List App</h1>
+    </header>
+
+
+    <AddList />
+
+  </div>
+</template>
+
+<style scoped>
+  #app {
+  text-align: center;
+  margin-top: 20px;
+  background: none;
+  padding: 10px;
+  border-radius: 10px;
+  border: 2px solid #2baaff;
+  width: 100%;
+}
+</style>
---
+++ src/components/AddList.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref,computed } from 'vue'
+
+const message = ref('')
+const itemList = ref([])
+
+const addItem = () => {
+  if(message.value.trim() !== ''){
+    itemList.value.push(message.value)
+    message.value=''
+  }
+}
+</script>
+
+<template>
+  <div class="main">
+    <div class="input-div">
+       <input v-model="message" placeholder="edit me" />
+      <button @click="addItem">Add</button>
+    </div>
+
+    <h2>Item List</h2>
+    <ul>
+      <li v-for="(item, index) in itemList" :key="index">{{item}}</li>
+    </ul>
+  </div>
+
+</template>
+
+<style scoped>
+  main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: blue;
+    border: 1px solid black;
+  }
+
+
+  ul{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    list-style-type: none;
+    padding: 20px;
+    margin: 10px;
+  }
+
+  li{
+    height: auto;
+    background: white;
+    color: blue;
+    margin: 10px;
+    border-radius: 5px;
+  }
+</style>
